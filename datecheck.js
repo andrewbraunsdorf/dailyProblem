dadae1b7a5fda3ff628942b529c053587f34e177
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-07-30 20:16:36 -0400

    added tofixed(2)

--- datecheck.js
+++ datecheck.js
@@ -8,7 +8,7 @@ function datecheck(m, y) {
     return false;
   }
   let currentDate = new Date();
-  let currentMonth = currentDate.getMonth();
+  let currentMonth = currentDate.getMonth() + 1;
   let currentYear = currentDate.getFullYear();
 
   let monthDecimal = m / 12;
@@ -17,11 +17,12 @@ function datecheck(m, y) {
   let newDate = currentYear + newMonthDecimal;
   let timeBetween = newDate - oldDate;
 
-  console.log(timeBetween);
+  console.log(timeBetween.toFixed(2));
 }
 
-datecheck(10, 1900);
-datecheck(-1, 2019);
-datecheck(0, 2020);
-datecheck(12, 1820);
-datecheck(11, 1990);
+datecheck(07, 2018);
+// datecheck(-1, 2019);
+// datecheck(0, 2020);
+// datecheck(12, 1820);
+// datecheck(11, 1990);
+console.log(Date());
