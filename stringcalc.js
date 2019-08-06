260c73d24f9bbbe3b4dcf512c16e4f9d68ba899e
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-08-06 19:10:04 -0400

    completed it without eval()

--- stringcalc.js
+++ stringcalc.js
@@ -1,10 +1,57 @@
-function evaulate(mathProblem) {
-  let answer = eval(mathProblem);
-  console.log(answer);
-}
+// function evaulate(mathProblem) {
+//   let answer = eval(mathProblem);
+//   console.log(answer);
+// }
 
-evaulate("10 + 13 - 5");
-evaulate("10 - 13 * 5");
-evaulate("10 * 13 / 3");
-evaulate("10 / 13 * 5");
-evaulate("13 % 10 % 2");
+let addition = ("10 + 13");
+
+function evaulate(mathProblem) {
+  let answer = mathProblem.split(" ");
+  // let newAnswer = 0;
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i] == "+") {
+      let result = parseInt(answer[0], 10) + parseInt(answer[2], 10);
+      console.log(result);
+    }
+    if (answer[i] == "-") {
+      let result = parseInt(answer[0], 10) - parseInt(answer[2], 10);
+      console.log(result);
+    }
+    if (answer[i] == "*") {
+      let result = parseInt(answer[0], 10) * parseInt(answer[2], 10);
+      console.log(result);
+    }
+    if (answer[i] == "/") {
+      let result = parseInt(answer[0], 10) / parseInt(answer[2], 10);
+      console.log(result);
+    }
+    if (answer[i] == "x") {
+      let result = parseInt(answer[0], 10) * parseInt(answer[2], 10);
+      console.log(result);
+    }
+    if (answer[i] == "%") {
+      let result = parseInt(answer[0], 10) % parseInt(answer[2], 10);
+      console.log(result);
+    }
+  }
+}
+// if (answer[i] === "+" || "-") {
+// if (answer[i] == "+") {
+//   newAnswer += parseInt(answer[0], 10) + parseInt(answer[2], 10);
+//   console.log(newAnswer);
+// } else if (answer[i] == "-") {
+//   newAnswer += parseInt(answer[0], 10) - parseInt(answer[2], 10);
+//   console.log(newAnswer);
+// }
+// }
+
+evaulate(addition);
+evaulate("10 - 13");
+evaulate("10 * 13");
+evaulate("10 / 13");
+evaulate("13 % 10");
+evaulate("13 x 10");
+  // evaulate("10 - 13 * 5");
+  // evaulate("10 * 13 / 3");
+  // evaulate("10 / 13 * 5");
+  // evaulate("13 % 10 % 2");
