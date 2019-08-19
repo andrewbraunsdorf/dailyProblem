3ab17a098f8225cee2a4739dafc39b059ecdaa77
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-08-19 19:49:51 -0400

    added readline

--- yahtzee.js
+++ yahtzee.js
@@ -5,24 +5,30 @@
 // allow the user to give you multiple dice to reroll( same as above )
 // allow to roll a set number of dice
 // allow the player to roll the dice 3 times after each roll ask the player for dice to reroll and reroll the dice
+// display only the newly rolled dice
+// which rolls that were changed with the original dice
+
+// bonus if you get all five numbers yahtzee
+const readline = require("readline");
+
+let rollNumber = 0;
+let rollArray = rollFiveDie();
 
 function rolldice() {
   let roll = Math.floor(Math.random() * 6) + 1;
   return roll;
 };
 
-
-
 function rollFiveDie() {
   let arrayOfRolls = [];
   for (let rollTimes = 0; rollTimes < 5; rollTimes++) {
     arrayOfRolls.push(rolldice())
   }
-  console.log(arrayOfRolls);
+  return arrayOfRolls;
 }
 
 function rolldiceThreeTimes() {
-  let dice = []
+  let dice = [];
   for (let eachDice = 0; eachDice < 3; eachDice++) {
     // dice[eachDice] = dice.push(rollFiveDie());
     // console.log(dice[eachDice]);
@@ -30,14 +36,37 @@ function rolldiceThreeTimes() {
   }
   // console.log(dice[0]);
 }
+function yahtzee() {
+  rollNumber++;
+  if (rollNumber == 4) {
+    return 0;
+  }
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
+  rl.question(`dice: [${rollArray}]\n Input the index of dice to reroll (1-5) `, (answer) => {
+    console.log(`You want to roll ${answer} dice`);
 
-rolldice()
-// rolldice()
-// rolldice()
-// rolldice()
-// rolldice()
-// rolldice()
+    rl.close();
+  });
+  // rl.question('Enter the dice you want to roll2', (answer) => {
+  //   // TODO: Log the answer in a database
+  //   console.log(`You want to roll ${answer} dice`);
 
-rollFiveDie()
-rolldiceThreeTimes()
+  //   rl.close();
+  // });
+}
+
+// rolldice()
+// // rolldice()
+// // rolldice()
+// // rolldice()
+// // rolldice()
+// // rolldice()
+
+// rollFiveDie()
+// rolldiceThreeTimes()
+
+yahtzee();
