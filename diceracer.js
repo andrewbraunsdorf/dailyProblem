ed05033c4216efb18f32854764d465caf29e8e4f
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-08-27 19:07:18 -0400

    shuffle deck

--- diceracer.js
+++ diceracer.js
@@ -18,6 +18,20 @@ function createDeck() {
     }
   }
 
+  var m = deckOfCards.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = deckOfCards[m];
+    deckOfCards[m] = deckOfCards[i];
+    deckOfCards[i] = t;
+  }
+
   // return deckOfCards;
   console.log(deckOfCards);
 }
