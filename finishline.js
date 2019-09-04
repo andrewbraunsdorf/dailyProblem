12f9dd003159f7746ddacfd9b3f5a590e1aa7ab6
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-04 19:28:19 -0400

    added bad cards to deck

--- finishline.js
+++ finishline.js
@@ -5,13 +5,42 @@ function createDeck() {
   let deckGood = {
     // suits: [0, 1, 2, 3],
     suits: ['Hearts', 'Diamonds', 'Clubs', 'Spades'],
-    card: [3, 4, 5, 6, 7, 8, 9, 10],
+    card: [3, 4, 5, 6, 7, 8, 9, 10]
   }
   for (let i = 0; i < deckGood.suits.length; i++) {
     for (j = 0; j < deckGood.card.length; j++) {
       let object = {
         suit: deckGood.suits[i],
         card: deckGood.card[j]
+      }
+      deckOfCards.push(object);
+    }
+  }
+  let m = deckOfCards.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = deckOfCards[m];
+    deckOfCards[m] = deckOfCards[i];
+    deckOfCards[i] = t;
+  }
+
+  tempDeckOfGoodCards = deckOfCards.slice(0, 4);
+  let deckBad = {
+    // suits: [0, 1, 2, 3],
+    suits: ['Hearts', 'Diamonds', 'Clubs', 'Spades'],
+    card: [2, 11, 12, 13, 14],
+  }
+  for (let i = 0; i < deckBad.suits.length; i++) {
+    for (j = 0; j < deckBad.card.length; j++) {
+      let object = {
+        suit: deckBad.suits[i],
+        card: deckBad.card[j]
       }
       deckOfCards.push(object);
     }
