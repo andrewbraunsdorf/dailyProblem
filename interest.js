5ae922ceaac7767974e2b0cc1fe9c2f8a63bc117
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-05 19:51:17 -0400

    added periodic deposits

--- interest.js
+++ interest.js
@@ -1,12 +1,14 @@
-function calculateInterest(principal, numberofmonths, monthlypercentinterest, deposit) {
+function calculateInterest(principal, numberofmonths, monthlypercentinterest, depositAmount, periodicDeposits) {
   // let simpleInterest = principal * (1 + (monthlypercentinterest / 100));
   let compoundInterest = principal * (Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1);
 
-  let compoundInterestOfDeposits = deposit * (((Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1) / (monthlypercentinterest / 12 / 100)))
+  let compoundInterestOfDeposits = depositAmount * (((Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1) / (monthlypercentinterest / 12 / 100)));
 
-  let total = principal + compoundInterest + compoundInterestOfDeposits;
+  let compoundInterestOfPeriodicDeposits = depositAmount * (periodicDeposits / 12) * (((Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1) / (monthlypercentinterest / 12 / 100)));
+
+  let total = principal + compoundInterest + compoundInterestOfPeriodicDeposits;
   console.log(compoundInterest);
   console.log(compoundInterestOfDeposits);
   console.log(total);
 }
-calculateInterest(5000, 120, 5, 100);
+calculateInterest(5000, 120, 5, 100, 4);
