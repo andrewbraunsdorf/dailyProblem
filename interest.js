b65ab1865943b420cf72fcf0ddb12a45c81ab3f7
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-05 19:42:55 -0400

    added deposits

--- interest.js
+++ interest.js
@@ -1,6 +1,12 @@
-function calculateInterest(principal, numberofmonths, monthlypercentinterest) {
+function calculateInterest(principal, numberofmonths, monthlypercentinterest, deposit) {
   // let simpleInterest = principal * (1 + (monthlypercentinterest / 100));
   let compoundInterest = principal * (Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1);
+
+  let compoundInterestOfDeposits = deposit * (((Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1) / (monthlypercentinterest / 12 / 100)))
+
+  let total = principal + compoundInterest + compoundInterestOfDeposits;
   console.log(compoundInterest);
+  console.log(compoundInterestOfDeposits);
+  console.log(total);
 }
-calculateInterest(1000, 24, 2);
+calculateInterest(5000, 120, 5, 100);
