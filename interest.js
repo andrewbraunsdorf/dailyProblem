33960f62e3e01a221c565a7878170a7b0713dfde
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-05 19:15:06 -0400

    added compound interest

--- interest.js
+++ interest.js
@@ -1,5 +1,6 @@
 function calculateInterest(principal, numberofmonths, monthlypercentinterest) {
-  let simpleInterest = principal * (1 + (monthlypercentinterest / 100));
-  console.log(simpleInterest);
+  // let simpleInterest = principal * (1 + (monthlypercentinterest / 100));
+  let compoundInterest = principal * (Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1);
+  console.log(compoundInterest);
 }
 calculateInterest(1000, 24, 2);
