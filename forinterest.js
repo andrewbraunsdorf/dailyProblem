54ef0c3868a096a03276db61dd6f2dea5ccb54b8
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-06 19:37:47 -0400

    added withdrawals

--- forinterest.js
+++ forinterest.js
@@ -36,6 +36,35 @@ function periodicDeposits(deposit, month) {
   }
 }
 
+function withdrawl(deposit, month, withdrawl, monthWithdrawl) {
+  let monthlyBalance = principal;
+  for (i = 0; i < months; i++) {
+    if (i % month == 0) {
+      if (i % monthWithdrawl == 0) {
+        monthlyBalance -= withdrawl
+        let monthlyInterest = monthlyBalance * (interest / 12 / 100)
+        monthlyBalance += monthlyInterest + deposit;
+        console.log(monthlyBalance);
+      } else {
+        let monthlyInterest = monthlyBalance * (interest / 12 / 100)
+        monthlyBalance += monthlyInterest;
+        console.log(monthlyBalance);
+      }
+    } else {
+      if (i % monthWithdrawl == 0) {
+        monthlyBalance -= withdrawl
+        let monthlyInterest = monthlyBalance * (interest / 12 / 100)
+        monthlyBalance += monthlyInterest + deposit;
+        console.log(monthlyBalance);
+      } else {
+        let monthlyInterest = monthlyBalance * (interest / 12 / 100)
+        monthlyBalance += monthlyInterest;
+        console.log(monthlyBalance);
+      }
+    }
+  }
+}
+
 // deposit(100);
-periodicDeposits(100, 1);
+withdrawl(100, 1, 50, 1);
 
