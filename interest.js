fbe82ecd868629e0f8123bdfc086a7af3222bb8f
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-05 20:27:52 -0400

    added withdrawals

--- interest.js
+++ interest.js
@@ -1,14 +1,25 @@
-function calculateInterest(principal, numberofmonths, monthlypercentinterest, depositAmount, periodicDeposits) {
+function calculateInterest(principal, numberofmonths, monthlypercentinterest, depositAmount, periodicDeposits, withdrawalAmount, periodicWithdrawal) {
+  let interestCalculation = (Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1);
+  let monthlyInterest = (monthlypercentinterest / 12 / 100);
+
   // let simpleInterest = principal * (1 + (monthlypercentinterest / 100));
-  let compoundInterest = principal * (Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1);
+  let compoundInterest = principal * interestCalculation;
 
-  let compoundInterestOfDeposits = depositAmount * (((Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1) / (monthlypercentinterest / 12 / 100)));
+  let compoundInterestOfDeposits = depositAmount * ((interestCalculation / monthlyInterest));
 
-  let compoundInterestOfPeriodicDeposits = depositAmount * (periodicDeposits / 12) * (((Math.pow(1 + (monthlypercentinterest / 12 / 100), numberofmonths) - 1) / (monthlypercentinterest / 12 / 100)));
+  let compoundInterestOfPeriodicDeposits = depositAmount * (periodicDeposits / 12) * ((interestCalculation / monthlyInterest));
+
+  // let withdrawalWithPeriodicWithdrawal = -withdrawalAmount * (periodicWithdrawal / 12) * ((interestCalculation / monthlyInterest));
+
+  let withdrawalWithPeriodicWithdrawal = -withdrawalAmount * ((periodicWithdrawal / 12) * ((interestCalculation / monthlyInterest))) * (1 + monthlyInterest);
 
   let total = principal + compoundInterest + compoundInterestOfPeriodicDeposits;
+
+  let totalWithWithdrawal = principal + compoundInterest + withdrawalWithPeriodicWithdrawal;
   console.log(compoundInterest);
   console.log(compoundInterestOfDeposits);
   console.log(total);
+
+  console.log(totalWithWithdrawal);
 }
-calculateInterest(5000, 120, 5, 100, 4);
+calculateInterest(5000, 1, 5, 0, 0, 100, 12);
